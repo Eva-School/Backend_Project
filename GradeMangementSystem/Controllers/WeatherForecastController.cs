57f9f00096d49aef487b3d085ff0ee07eb3a6bdd
Abdulrahman Mahmoud <bm9412238@gmail.com>
2025-12-24 21:10:27 +0200

    Add gitignore for Visual Studio files

--- GradeMangementSystem/Controllers/WeatherForecastController.cs
+++ GradeMangementSystem/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GradeMangementSystem.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Abdo", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
